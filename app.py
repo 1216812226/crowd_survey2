917fdf3e12f15c6d6449859ce68060aac52aa64b
1216812226 <107613629+1216812226@users.noreply.github.com>
2026-09-09 19:12:46 +0100

    Add Q7 and Q8 fields to form handling

--- app.py
+++ app.py
@@ -42,6 +42,8 @@ def init_db():
             Q5 TEXT,
             Q5_reason TEXT,
             Q6_add TEXT,
+            Q7 TEXT,
+            Q8 TEXT,
             Q9 TEXT,
             Q9reason TEXT,
             Q10 TEXT,
@@ -83,6 +85,8 @@ def save_to_database(data):
             Q5,
             Q5_reason,
             Q6_add,
+            Q7,
+            Q8,
             Q9,
             Q9reason,
             Q10,
@@ -107,6 +111,8 @@ def save_to_database(data):
             %(Q5)s,
             %(Q5_reason)s,
             %(Q6_add)s,
+            %(Q7)s,
+            %(Q8)s,
             %(Q9)s,
             %(Q9reason)s,
             %(Q10)s,
@@ -165,6 +171,10 @@ def survey():
             ),
 
             'Q6_add': request.form.get('Q6_add'),
+
+            'Q7': request.form.get('Q7'),
+
+            'Q8': request.form.get('Q8'),
 
             'Q9': ', '.join(
                 request.form.getlist('Q9')
